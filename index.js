bd2106d093434f3992d6c4fbb86b25d8da794ee9
Nathan Moinvaziri <nathan@nathanm.com>
2022-06-16 09:31:32 -0700

    Fixed more whitespace formatting in try/catch statement.

--- index.js
+++ index.js
@@ -24,29 +24,30 @@ function walk_recursive(dir, exclude, callback) {
 }
 
 try {
-    const regex = new RegExp(core.getInput('regex'), core.getInput('flags'));
-    const replacement = core.getInput('replacement');
-    const include = new RegExp(core.getInput('include'));
-    const exclude = new RegExp(core.getInput('exclude'));
-    const encoding = core.getInput('encoding');
-    const start_path = core.getInput('path');
+  const regex = new RegExp(core.getInput('regex'), core.getInput('flags'));
+  const replacement = core.getInput('replacement');
+  const include = new RegExp(core.getInput('include'));
+  const exclude = new RegExp(core.getInput('exclude'));
+  const encoding = core.getInput('encoding');
+  const start_path = core.getInput('path');
 
-    walk_recursive(start_path, exclude, (file) => {
-      if (include.test(file)){
-        fs.readFile(file, encoding, function (err,data) {
-          if (err) {
-            return console.log(err);
-          }
-          var result = data.replace(regex, replacement);
-          if (data != result) {
-            console.log('Modifying ', file);
-            fs.writeFile(file, result, encoding, function (err) {
-            if (err) return core.setFailed(err);
-           });
-          }
-        });
-      }
-    })
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+  walk_recursive(start_path, exclude, (file) => {
+    if (include.test(file)){
+      fs.readFile(file, encoding, function (err, data) {
+        if (err) {
+          return console.log(err);
+        }
+        var result = data.replace(regex, replacement);
+        if (data != result) {
+          console.log('Modifying ', file);
+          fs.writeFile(file, result, encoding, function (err) {
+            if (err)
+              return core.setFailed(err);
+          });
+        }
+      });
+    }
+  })
+} catch (error) {
+  core.setFailed(error.message);
+}
